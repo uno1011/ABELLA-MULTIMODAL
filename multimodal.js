6e321fa515f7050d6de5b541466df7200268b2d5
uno1011 <151750618+uno1011@users.noreply.github.com>
2025-10-25 20:57:54 +0800

    Delete multimodal.js

--- multimodal.js
+++
@@ -1,7 +0,0 @@
-const title = document.getElementById("title");
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-  title.textContent = "Button Clicked!";
-  title.style.color = "yellow";
-});
